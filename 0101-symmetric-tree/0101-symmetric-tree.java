813bb9d092909ce77cf4c50b14da22631d81b28a
AdelineXu-SDE <167590293+AdelineXu-SDE@users.noreply.github.com>
2025-03-11 14:56:54 -0600

    Time: 0 ms (100%), Space: 41.1 MB (99.85%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,18 +15,16 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        //DS: Tree
-        //A: DFS + recursion
         if(root == null) return true;
-        return isMirror(root.left, root.right);  
+
+        return isMirror(root.left, root.right); 
     }
 
     private boolean isMirror(TreeNode left, TreeNode right){
         if(left == null && right == null) return true;
         if(left == null || right == null) return false;
-        if(left.val != right.val) return false;
 
-        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+
     }
-
 }
