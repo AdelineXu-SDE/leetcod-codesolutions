a0b7261e91e3e0e17c86e8420015ef58478b535a
AdelineXu-SDE <167590293+AdelineXu-SDE@users.noreply.github.com>
2025-03-15 12:01:13 -0600

    Time: 2 ms (84.8%), Space: 46.2 MB (19.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -7,9 +7,10 @@ class Solution {
         int first = 0;
         for(int second = 0; second < nums.length; second++){
             if(nums[second] != 0){
-                int temp = nums[second];
-                nums[second] = nums[first];
-                nums[first] = temp;
+                if(first != second){
+                    nums[first] = nums[second];
+                    nums[second] = 0;
+                }
                 first++;
             }
         } 
