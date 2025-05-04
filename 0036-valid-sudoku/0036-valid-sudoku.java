6965f5368a22e0c12d0751d893f32cfe963c6763
AdelineXu-SDE <167590293+AdelineXu-SDE@users.noreply.github.com>
2025-05-04 10:08:46 -0600

    Time: 3 ms (55.61%), Space: 44.6 MB (50.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,29 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int n = board.length;
-        int squareLen = board[0].length;
+        int n = board[0].length;
 
         Set<Character>[] rows = new HashSet[9];
         Set<Character>[] cols = new HashSet[9];
         Set<Character>[] boxes = new HashSet[9];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < 9; i++){
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();    
+            boxes[i] = new HashSet<>();
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 char val = board[i][j];
-                if(val == '.') continue;
-
-                if(rows[i].contains(val) || cols[j].contains(val) || boxes[(i / 3) * 3 + j / 3].contains(val)){
+                if(board[i][j] == '.') continue;
+                if(rows[i].contains(val) || cols[j].contains(val) || boxes[(i / 3) * 3 + (j / 3)].contains(val)){
                     return false;
                 }
                 rows[i].add(val);
                 cols[j].add(val);
-                boxes[(i/3)*3 + j/3].add(val);
+                boxes[(i / 3) * 3 + j / 3].add(val);
             }
         }
         return true;
