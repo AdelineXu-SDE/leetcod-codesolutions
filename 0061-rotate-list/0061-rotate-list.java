6bb328c7e0905b0f5800aed0cc98560ae6cf1e18
AdelineXu-SDE <167590293+AdelineXu-SDE@users.noreply.github.com>
2025-05-22 12:32:17 -0600

    Time: 0 ms (100%), Space: 42.9 MB (17.26%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,27 +10,28 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        //DS: Linked List
-        //A: 
         if(head == null || k == 0 || head.next == null) return head;
 
-        int size = 1;
         ListNode tail = head;
+        int size = 1;
+
         while(tail.next != null){
             tail = tail.next;
             size++;
         }
-
+        
         k = k % size;
         if(k == 0) return head;
+
+        tail.next = head;
 
         ListNode newTail = head;
         for(int i = 0; i < size - k - 1; i++){
             newTail = newTail.next;
         }
+
         ListNode newHead = newTail.next;
         newTail.next = null;
-        tail.next = head;
 
         return newHead;
         
